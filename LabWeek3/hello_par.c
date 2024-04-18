9bd5ded3785b37099668582e867fc20f4cc74a9c
alifzwan <alifzakwan529@gmail.com>
2024-04-18 15:44:53 +0800

    Refactor hello_par.c to use standardized MPI variable names and update display message

--- LabWeek3/hello_par.c
+++ LabWeek3/hello_par.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 #include <mpi.h>
 
-//*    MPI:
-//?  - Its a standardized and portable message-passing system 
-//?    designed to function on a wide variety of parallel computing architectures.
+// Refer to the hello_par.c code, modify the code as follows:-
+// •	Given variable for MPI_Comm_size is size
+// •	Given variable for MPI_Comm_rank is p
+// •	Given display for printf is “ Greetings from process __ and total number of processors is __. I am running on __”
 
 int main(int argc, char *argv[]) {
 
-
-    // numprocs - number of processes
-    // rank     - rank of the calling process in the communicator
-    // namelen  - length of the name
-    int size, p, namelen;
-
-    
+    int size, p, namelen; //number of processes, rank of the calling process in the communicator, length of the name
     char processor_name[MPI_MAX_PROCESSOR_NAME];  // name of the processor
 
     //! Initialize the MPI execution environment
     MPI_Init(&argc, &argv); // Initialize the MPI execution environment
 
-    
         MPI_Comm_size(MPI_COMM_WORLD, &size); // Get the number of processes
-
         
         MPI_Comm_rank(MPI_COMM_WORLD, &p); // Get the rank of the calling process in the communicator
-
         
         MPI_Get_processor_name(processor_name, &namelen); // Get the name of the processor
 
