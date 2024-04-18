f9838e5bbf4d2a8ee65487b7b72be7e4af3d15bb
alifzwan <alifzakwan529@gmail.com>
2024-04-18 14:24:38 +0800

    Refactor hello_par.c to include MPI initialization and finalize functions

--- LabWeek3/hello_par.c
+++ LabWeek3/hello_par.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include <mpi.h>
 
+//*    MPI:
+//?  - Its a standardized and portable message-passing system 
+//?    designed to function on a wide variety of parallel computing architectures.
+
 int main(int argc, char *argv[]) {
-int numprocs, rank, namelen;
-char processor_name[MPI_MAX_PROCESSOR_NAME];
 
-MPI_Init(&argc, &argv);
-MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-MPI_Get_processor_name(processor_name, &namelen);
-printf("Process %d on %s out of %d\n", rank, processor_name, numprocs);
 
- MPI_Finalize();}
+    // numprocs - number of processes
+    // rank     - rank of the calling process in the communicator
+    // namelen  - length of the name
+    int size, p, namelen;
+
+    
+    char processor_name[MPI_MAX_PROCESSOR_NAME];  // name of the processor
+
+    //! Initialize the MPI execution environment
+    MPI_Init(&argc, &argv); // Initialize the MPI execution environment
+
+    
+        MPI_Comm_size(MPI_COMM_WORLD, &size); // Get the number of processes
+
+        
+        MPI_Comm_rank(MPI_COMM_WORLD, &p); // Get the rank of the calling process in the communicator
+
+        
+        MPI_Get_processor_name(processor_name, &namelen); // Get the name of the processor
+
+        // print the rank, processor name and number of processes
+        printf("Greetings from process %d and total number of processors is %d. I am running on %s\n", p, size, processor_name); 
+
+    
+    MPI_Finalize(); // Terminate the MPI execution environment
+    //! Finalize the MPI execution environment
+ }
