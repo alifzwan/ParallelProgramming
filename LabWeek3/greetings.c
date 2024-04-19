f7636606ae4fdb3c7949d2a1744f2bd4985b161b
alifzwan <alifzakwan529@gmail.com>
2024-04-19 12:05:17 +0800

    Refactor MPI communication in greetings.c and hello_par.c for better consistency and readability

--- LabWeek3/greetings.c
+++ LabWeek3/greetings.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 #include "mpi.h"
+
 // Refer to the greetings.c code:-
 // •	Write the code.
 // •	Successfully compiled your code.
 // •	Understand the output
-int main(int argc, char* argv[]) 
-{
-    int         my_rank, p, source, dest, tag=0;  //rank of the calling process in the communicator, number of processes
-                                                  //rank of the sender, rank of the receiver, message tag
-    char        message[100];  // storage for message  
 
-    MPI_Status  status;        // return status for  receive     
+int main(int argc, char* argv[]){
+
+    int         my_rank, p, source, dest, tag=0;                                                 
+    char        message[100];   
+
+    MPI_Status  status;       // return status for  receive     
     MPI_Init(&argc, &argv);   // Start up MPI 
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);      // Find out process rank  
-    MPI_Comm_size(MPI_COMM_WORLD, &p);       // Find out number of processes 
-    if (my_rank != 0) {
-        sprintf(message, "Greetings from process %d!", my_rank); // Create message
-        dest = 0;
-        MPI_Send(message, strlen(message)+1, MPI_CHAR, dest, tag, MPI_COMM_WORLD); // Use strlen+1 so that '\0' gets transmitted 
+    MPI_Comm_size(MPI_COMM_WORLD, &p);           // Find out number of processes 
+
+    //! Process:
+    //* 1. Check if rank of current process is not 0
+    //* 2. If yes it'll create a message "Greetings from process %d!" 
+    //* 3. then set the rank receiver (dest) to 0  
+    //* 4. and send the message to the rank receiver which is rank 0
+    //* 5. if rank of current process is 0, it'll loop through all the processes
+    //* 6. and receive the message from the sender
+
+    if (my_rank != 0) { //* (1)
+
+        sprintf(message, "Greetings from process %d!", my_rank);  //* (2)
+        dest = 0; //* (3)
+        MPI_Send(message, strlen(message)+1, MPI_CHAR, dest, tag, MPI_COMM_WORLD); //* (4)
+
     } else {      
-        for (source = 1; source < p; source++){ // my_rank == 0
-            MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
+        //* (5)
+        for (source = 1; source < p; source++){ 
+            MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status); //* (6)
             printf("Test %s\n", message);
         }
+
     }
     MPI_Finalize();
 } 
--- LabWeek3/hello_par.c
+++ LabWeek3/hello_par.c
@@ -6,16 +6,29 @@
 // •	Given variable for MPI_Comm_rank is p
 // •	Given display for printf is “ Greetings from process __ and total number of processors is __. I am running on __”
 
-// argc - number of command line arguments passed into the program
+// argc   - number of command line arguments passed into the program
 // argv[] - array of command line arguments passed into the program
 
 int main(int argc, char *argv[]) {
 
-    int size, p, namelen; //number of processes, rank of the calling process in the communicator, length of the name
-    char processor_name[MPI_MAX_PROCESSOR_NAME];  // name of the processor
+        // size - number of processes
+        // p - rank of the calling process in the communicator
+        // namelen - length of the name
+        // processor_name - name of the processor
 
-    //! Initialize the MPI execution environment
-    MPI_Init(&argc, &argv); // Initialize the MPI execution environment
+        int size, p, namelen; 
+        char processor_name[MPI_MAX_PROCESSOR_NAME];  
+
+        //! Process:
+        //* 1. Initialize the MPI execution environment
+        //* 2. Get the number of processes
+        //* 3. Get the rank of the calling process in the communicator
+        //* 4. Get the name of the processor
+        //* 5. Print the rank, processor name and number of processes
+        //* 6. Terminate the MPI execution environment
+
+    
+        MPI_Init(&argc, &argv); 
 
         MPI_Comm_size(MPI_COMM_WORLD, &size); // Get the number of processes
         
@@ -27,6 +40,6 @@ int main(int argc, char *argv[]) {
         printf("Greetings from process %d and total number of processors is %d. I am running on %s\n", p, size, processor_name); 
 
     
-    MPI_Finalize(); // Terminate the MPI execution environment
-    //! Finalize the MPI execution environment
+        MPI_Finalize(); // Terminate the MPI execution environment
+    
  }
