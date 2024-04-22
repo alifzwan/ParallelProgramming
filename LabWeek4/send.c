9aa99733239989793df47b54506e5eedb8f3e3ba
alifzwan <alifzakwan529@gmail.com>
2024-04-22 16:08:02 +0800

    Refactor MPI communication in send.c for better consistency and readability

--- LabWeek4/send.c
+++ LabWeek4/send.c
@@ -14,7 +14,7 @@ int main(argc,argv) int argc; char *argv[]; {
 
     MPI_Init(&argc,&argv); // Initialize the MPI environment
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks); // Get the number of processes
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Get the rank of the process
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);     // Get the rank of the process
 
 
     //! Process: This code demonstrates sending and receiving messages between two processes.
@@ -26,25 +26,25 @@ int main(argc,argv) int argc; char *argv[]; {
     //* 6 - Get the count of the message received for printing the message 
     //* 7 - Print the message received from the sender
 
-    if (rank == 0){ //* (1)
+    if (rank == 0){ //* (1) - Check if the rank of current process is 0
 
-        dest = 1; //* (2) 
+        dest = 1; //* (2) - if it does, it sets rank of receiver (dest) to 1
         source = 1; 
-        MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD); //* (3)
+        MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD); //* (3)- Then, it send message to Rank 1
         MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
 
-    } else if (rank == 1) { //* (4)
+    } else if (rank == 1) { //* (4) - if the rank is not 0, it sets rank of receiver (dest) to 0
 
         dest = 0; 
         source = 0; 
-        MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat); //* (5)
+        MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat); //* (5) - Then, it receives message from Rank 0
         MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD); 
 
     }
 
-    MPI_Get_count(&Stat, MPI_CHAR, &count); //* (6)
+    MPI_Get_count(&Stat, MPI_CHAR, &count); //* (6) - Get the count of the message received for printing the message
     
-    
-    printf("Task %d: Received %d char(s) from task %d with tag %d \n", rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG); //* (7)
+    //* (7) - Print the message received from the sender
+    printf("Task %d: Received %d char(s) from task %d with tag %d \n", rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG); 
     MPI_Finalize(); 
 }
