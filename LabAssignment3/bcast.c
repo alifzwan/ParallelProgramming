79b9e40294dec24c510d64400828514a71dec2c3
alifzwan <alifzakwan529@gmail.com>
2024-05-20 14:32:27 +0800

    Refactor bcast.c for better consistency and readability

--- LabAssignment3/bcast.c
+++ LabAssignment3/bcast.c
@@ -2,10 +2,10 @@
 #include <mpi.h>
 
 int main(int argc, char **argv){
-    int size, node, i, x;
-    double MPI_Wtime (void);
-    double t1, t2;
-    int a, b;
+    int size, node, i, x; // declare the variables size, node, i, x
+    double MPI_Wtime (void); // declare the function MPI_Wtime 
+    double t1, t2; // declare the variables t1, t2
+    int a, b; // declare the variables a, b
 
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -14,11 +14,15 @@ int main(int argc, char **argv){
     if(node == 0){ // if the rank of the current processor is 0
         a = 20000*30000; // set the value of a to 20000*30000
         printf("I am node %d and  a is %d \n", node, a); // print the rank of the current processor and the value of a
+
+      //! We cannot put MPI_Bcast in the bracket of if(node == 0) because it will not be executed by other processors except 0 
+      //? MPI_Bcast(&a, 1, MPI_INT, 0, MPI_COMM_WORLD);   
     }
+
 
     printf("Before Bcast : Node %d  and a = %d \n", node, a); // print the rank of the current processor and the value of a
 
-    MPI_Bcast(&a, 1, MPI_INT, 0, MPI_COMM_WORLD); // broadcast the value of a to all processors
+    MPI_Bcast(&a, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     t1 = MPI_Wtime(); // get the current time
 
