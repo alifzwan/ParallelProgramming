e2a60206559905ffaaeb87c8f2acfd0d737c858d
alifzwan <alifzakwan529@gmail.com>
2024-06-12 15:12:19 +0800

    refactor: Update MPI communication in task2.c for consistency and readability

--- LabAssignment4/task2.c
+++ LabAssignment4/task2.c
@@ -6,30 +6,31 @@
 // Then using MPI_Bcast, broadcast the value of a to processors 1, 2, 3, 4 and 5. 
 
 int main(int argc, char* argv[]) {
-    int my_rank, size;  // rank of process and number of processes 
-    int a = 0;
+    int node, size;
+    double a;
     int b = 4, c = 4, d = 2;
-    
 
-    MPI_Init(&argc, &argv);                   // Start up MPI 
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);  // Find out process rank 
-    MPI_Comm_size(MPI_COMM_WORLD, &size);        // Find out number of processes 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &node);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Processor 0 calculates the value of a
-    if (my_rank == 0) {  
-        a = (b + c) / d;
-        printf("I am node 0 and a is %d\n", a);
-    } else {
-        printf("Before bcast\n");
-        printf("I am node %d and a is %d\n", my_rank, a);
+    if(node == 0) {
+        a = (double)(b + c) / d;
+        printf("I am node 0 and a is %f\n", a);
     }
 
-    // Broadcast the value of a from process 0 to all other processes
-    MPI_Bcast(&a, 1, MPI_INT, 0, MPI_COMM_WORLD); // Broadcast the value of a to all other processors
-
-    if (my_rank != 0) {
-        printf("After bcast\n");
-        printf("I am node %d and a is %d\n", my_rank, a);
+    if(node != 0) {
+        printf("Before Bcast:\n");
+        printf("I am node %d and a is %f\n", node, a);
     }
-    MPI_Finalize(); // Shut down MPI
+
+    MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if(node != 0) {
+        printf("After Bcast:\n");
+        printf("I am node %d and a is %f\n", node, a);
+    }
+
+    MPI_Finalize();
 }
+
