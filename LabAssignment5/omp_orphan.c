0b4ac271b23183f9f00c8eb2d0752f12513f37ff
alifzwan <alifzakwan529@gmail.com>
2024-06-26 14:54:09 +0800

    refactor to make the code readable

--- LabAssignment5/omp_orphan.c
+++ LabAssignment5/omp_orphan.c
@@ -7,51 +7,40 @@
 * AUTHOR: Blaise Barney  5/99
 * LAST REVISED: 06/30/05
 ******************************************************************************/
-
-
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define VECLEN 100
 
-// omp_orphan.c
-// - This program demonstrates an orphaned directive, 
-//   where the parallel directive is outside the function performing the work. 
-// - The reduction variable scope is crucial.
-
 float a[VECLEN], b[VECLEN], sum;
 
 float dotprod () {
-    int i, tid;
+  int i,tid;
 
-    tid = omp_get_thread_num();
+  tid = omp_get_thread_num();
 
-    #pragma omp for reduction(+:sum)
-
+  #pragma omp for reduction(+:sum)
+  {
     for (i = 0; i < VECLEN; i++) {
-        sum = sum + (a[i] * b[i]);
-        printf("tid= %d i=%d\n", tid, i);
+      sum = sum + (a[i] * b[i]);
+      printf("tid= %d i= %d\n", tid, i);
     }
+  }
 }
 
 
 int main (int argc, char *argv[]) {
-    int i;
+  int i;
 
-    for (i = 0; i < VECLEN; i++) {
-        a[i] = b[i] = 1.0 * i;
-    }
+  for (i=0; i < VECLEN; i++) {
+    a[i] = b[i] = 1.0 * i;
+  }
+  sum = 0.0;
 
-    sum = 0.0;
-
-    double start_time = omp_get_wtime();
-    #pragma omp parallel
+  #pragma omp parallel
+  {
     dotprod();
-    double end_time = omp_get_wtime();
-    
+  }
 
-    printf("Sum = %f\n", sum);
-    printf("Execution Time: %f seconds\n", end_time - start_time);
-
-    return 0;
+  printf("Sum = %f\n",sum);
 }
--- LabAssignment5/omp_reduction.c
+++ LabAssignment5/omp_reduction.c
@@ -5,44 +5,33 @@
 *   This example demonstrates a sum reduction within a combined parallel loop
 *   construct.  Notice that default data element scoping is assumed - there
 *   are no clauses specifying shared or private variables.  OpenMP will 
-*   automatically make loop index variables private within team threads and
+*   automatically make loop index variables private within team threads, and
 *   global variables shared.
 * AUTHOR: Blaise Barney  5/99
 * LAST REVISED: 04/06/05
 ******************************************************************************/
-
-
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// omp_reduction.c
-// - This program shows how to perform a reduction operation in a parallel loop. 
-// - The sum of the products of two arrays is calculated.
-
 int main (int argc, char *argv[]) {
-    int i, n=100; // i: loop index, n: number of iterations
-    float a[100], b[100], sum; // a, b: arrays of size 100, sum: sum of the products of a[i] and b[i]
+  int i, n = 100;
+  float a[100], b[100], sum; 
 
-    for (i = 0; i < n; i++) { // loop through the array
-        a[i] = b[i] = i * 1.0; // initialize a[i] and b[i] to i*1.0
+
+ 
+  for (i = 0; i < n; i++){
+    a[i] = b[i] = i * 1.0;
+  }
+  
+  sum = 0.0;
+
+  #pragma omp parallel for reduction(+:sum)
+  {
+    for (i=0; i < n; i++){
+      sum = sum + (a[i] * b[i]);
     }
-    sum = 0.0;
+  }
 
-    double start_time = omp_get_wtime();
-
-    // Parallel region
-    
-    // shared: a, b, n, sum
-    // #pragma omp parallel for reduction(+:sum) 
-    #pragma omp parallel for schedule(dynamic) reduction(+:sum)
-
-    for (i = 0; i < n; i++) {
-        sum = sum + (a[i] * b[i]);
-    }
-    double end_time = omp_get_wtime();
-
-    printf("Sum = %f\n", sum);
-    printf("Execution Time: %f seconds\n", end_time - start_time);
-    return 0;
+  printf("   Sum = %f\n",sum);
 }
