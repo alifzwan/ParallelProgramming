2bd69775eceadb11e56d1e09a470be314fbbd991
alifzwan <alifzakwan529@gmail.com>
2024-06-28 12:07:46 +0800

    refactor: Parallelize computation of sum of arrays using OpenMP

--- LabAssignment5/omp_orphan.c
+++ LabAssignment5/omp_orphan.c
@@ -10,37 +10,35 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define VECLEN 100
+#define VECLEN 100 // Define vector length
 
-float a[VECLEN], b[VECLEN], sum;
+float a[VECLEN], b[VECLEN], sum; // Declare variables
 
-float dotprod () {
-  int i,tid;
+float dotprod () { // Define function to perform dot product of two vectors a and b
+  int i, tid; // Declare variables
 
-  tid = omp_get_thread_num();
+  tid = omp_get_thread_num(); // Get thread number
 
-  #pragma omp for reduction(+:sum)
-  {
-    for (i = 0; i < VECLEN; i++) {
-      sum = sum + (a[i] * b[i]);
-      printf("tid= %d i= %d\n", tid, i);
+  #pragma omp for reduction(+:sum) // Start parallel region with reduction to sum
+    for (i = 0; i < VECLEN; i++) { // Loop through array of length VECLEN
+      sum = sum + (a[i] * b[i]); // Sum the product of a and b arrays
+      printf("tid = %d, i = %d\n", tid, i); // Print thread number and i
     }
-  }
 }
 
 
 int main (int argc, char *argv[]) {
-  int i;
+  int i; // Declare variable
 
-  for (i=0; i < VECLEN; i++) {
-    a[i] = b[i] = 1.0 * i;
-  }
-  sum = 0.0;
+  for (i = 0; i < VECLEN; i++) { // Loop through array of length VECLEN
+    a[i] = b[i] = 1.0 * i; // Initialize a and b arrays
+  } 
+  sum = 0.0; // Initialize sum
 
-  #pragma omp parallel
+  #pragma omp parallel // Start parallel region
   {
-    dotprod();
+    dotprod(); // Perform dot product
   }
 
-  printf("Sum = %f\n",sum);
+  printf("Sum = %f\n",sum); // Print the sum
 }
