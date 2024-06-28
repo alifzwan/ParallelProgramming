8339fd6f3e460c150f3d077fb397740b33fcc82b
alifzwan <alifzakwan529@gmail.com>
2024-06-28 12:07:12 +0800

    refactor: Parallelize computation of sum of arrays using OpenMP in loops of 2,4,8,16 nodes

--- LabAssignment5/omp_workshare1.c
+++ LabAssignment5/omp_workshare1.c
@@ -12,35 +12,43 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define CHUNKSIZE   10
-#define N       100
+#define CHUNKSIZE 10
+#define N 100
 
 int main (int argc, char *argv[]) {
-    int nthreads, tid, i, chunk;
+    int node, tid, i, chunk=CHUNKSIZE; // declare variables
     float a[N], b[N], c[N];
+    double start_time, end_time;
 
     
     for (i = 0; i < N; i++){
         a[i] = b[i] = i * 1.0;
     }
-    chunk = CHUNKSIZE;
 
-#pragma omp parallel shared(a,b,c,nthreads,chunk) private(i,tid)
-{
-    tid = omp_get_thread_num();
-    if (tid == 0){
-        nthreads = omp_get_num_threads();
-        printf("Number of threads = %d\n", nthreads);
+
+    for(int node = 2; node <= 16; node *= 2){
+        omp_set_num_threads(node); // Set number of threads
+        start_time = omp_get_wtime(); // Start timing
+
+        #pragma omp parallel shared(a, b, c, node, chunk) private(i, tid)
+        {
+            if (node == 0){
+                printf("Number of threads = %d\n", node);
+            }
+
+            printf("Thread %d starting...\n", node);
+
+            #pragma omp for schedule(static,chunk)
+            for (int i = 0; i < N; i++) {
+                c[i] = a[i] + b[i];
+                
+                printf("Thread %d: c[%d]= %f\n", node, i, c[i]);
+            }
+        } 
+    end_time = omp_get_wtime(); // End timing
+
+    printf("Execution time with %d threads: %f seconds\n", node, end_time - start_time); 
     }
 
-    printf("Thread %d starting...\n",tid);
-
-    #pragma omp for schedule(static,chunk)
-    for (i = 0; i < N; i++) {
-        c[i] = a[i] + b[i];
-        
-        printf("Thread %d: c[%d]= %f\n", tid, i, c[i]);
-    }
-  } 
 }
 
